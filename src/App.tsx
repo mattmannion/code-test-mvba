118d339e050a177d3fb715b66e9a196839e9723d
Matt Mannion <mattpmannion@gmail.com>
2022-10-04 01:48:35 -0400

    prettier ui

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,30 @@ interface Order {
   ingredients: Ings;
 }
 
+interface PizzaCard {
+  name: string;
+  order_num: number;
+  ingredients: string[];
+}
+
+function PizzaCard({ name, order_num, ingredients }: PizzaCard) {
+  return (
+    <div className='app__pizza-card'>
+      <div className='app__pizza-card-title'>
+        <div>Type: {name}</div>
+        <div> Order#: {order_num + 1}</div>
+      </div>
+      <div className='app__pizza-card-ings'>
+        {ingredients.map((ing, i) => (
+          <div className='app__pizza-card-ing' key={i}>
+            {ing}{' '}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 function ClearChecked(arr: string[], el_id: string) {
   arr.forEach((_, i) => {
     const cb = document.getElementById(el_id + i) as HTMLInputElement;
@@ -60,6 +84,18 @@ export function App() {
           }
         });
       });
+
+    Object.keys(pizzas).forEach((_, i) => {
+      const cb = document.getElementById(
+        'pizza-input-' + i
+      ) as HTMLInputElement;
+      const bg = document.getElementById(
+        'pizza-input-bg-' + i
+      ) as HTMLDivElement;
+
+      if (cb.checked) bg.classList.add('app__pizza--active');
+      else bg.classList.remove('app__pizza--active');
+    });
   }, [selection]);
 
   if (error)
@@ -72,6 +108,7 @@ export function App() {
   return (
     <div className='app'>
       <form
+        className='app__form'
         onSubmit={(e) => {
           e.preventDefault();
           if (selection.length > 0 && customIngs.length > 0) {
@@ -88,61 +125,73 @@ export function App() {
           }
         }}
       >
-        {Object.entries(pizzas).map(([pizza, ings], i) => (
-          <div key={i}>
-            <input
-              id={'pizza-input-' + i}
-              type='radio'
-              name={pizza}
-              value={pizza}
-              checked={selection === pizza}
-              onChange={(e) => {
-                if (e.target.checked) {
-                  setSelection(e.target.value);
-                  setCustomIngs(ings.sort());
+        <div className='app__pizzas-title'>Pizza Selection</div>
 
-                  ClearChecked(baseIngs, 'custom-ings-input-');
-                } else
-                  setCustomIngs((prev) =>
-                    prev.filter((item) => item === pizza).sort()
-                  );
-              }}
-            />
-            <label htmlFor={'pizza-input-' + i}>&nbsp;{pizza}</label>
-          </div>
-        ))}
-        <hr />
-        {baseIngs.map((ing, i) => {
-          return (
-            <div key={i}>
+        <div className='app__pizzas'>
+          {Object.entries(pizzas).map(([pizza, ings], i) => (
+            <div className='app__pizza' id={'pizza-input-bg-' + i} key={i}>
               <input
-                id={'custom-ings-input-' + i}
-                type='checkbox'
-                name={ing}
+                id={'pizza-input-' + i}
+                type='radio'
+                name={pizza}
+                value={pizza}
+                checked={selection === pizza}
                 onChange={(e) => {
-                  if (e.target.checked)
-                    setCustomIngs((prev) => [...prev, ing].sort());
-                  else
+                  if (e.target.checked) {
+                    setSelection(e.target.value);
+                    setCustomIngs(ings.sort());
+
+                    ClearChecked(baseIngs, 'custom-ings-input-');
+                  } else
                     setCustomIngs((prev) =>
-                      prev.filter((item) => item !== ing).sort()
+                      prev.filter((item) => item === pizza).sort()
                     );
                 }}
               />
-              <label htmlFor={'custom-ings-input-' + i}>&nbsp;{ing}</label>
+              <label htmlFor={'pizza-input-' + i}>&nbsp;{pizza}</label>
             </div>
-          );
-        })}
-        <button name='add to order' type='submit'>
-          add to order
-        </button>
+          ))}
+        </div>
+
+        <div className='app__ings-cont'>
+          <div className='app__ings'>
+            {baseIngs.map((ing, i) => {
+              return (
+                <div key={i} className='app__ing'>
+                  <input
+                    id={'custom-ings-input-' + i}
+                    type='checkbox'
+                    name={ing}
+                    onChange={(e) => {
+                      if (e.target.checked)
+                        setCustomIngs((prev) => [...prev, ing].sort());
+                      else
+                        setCustomIngs((prev) =>
+                          prev.filter((item) => item !== ing).sort()
+                        );
+                    }}
+                  />
+                  <label htmlFor={'custom-ings-input-' + i}>&nbsp;{ing}</label>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className='app__submit-btn'>
+          <button name='add to order' type='submit'>
+            add to order
+          </button>
+        </div>
       </form>
-      <div>
-        orders:
-        {orders.map((order, i) => (
-          <pre key={i} data-testid={'order-' + i}>
-            {JSON.stringify(order, null, 2)}
-          </pre>
-        ))}
+      <div className='app__orders'>
+        <div>Orders</div>
+        <div className='app__pizza-cards'>
+          {orders.map((order, i) => (
+            <div data-testid={'order-' + i} key={i}>
+              <PizzaCard order_num={i} {...order} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
